2ae7e1f36f3e0a502918da3a9d5a5b15d13a31e6
Yim_JunSu <yimjunsu@gmail.com>
2025-05-03 03:06:57 +0900

    project create

---
+++ frontend/src/main.jsx
@@ -0,0 +1,12 @@
+// src/main.jsx
+import './index.css'; // 위치 맞는지 확인!
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}", // JSX와 TSX 파일도 포함
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
